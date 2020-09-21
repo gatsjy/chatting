5e5b4b164b8228c857103712c9f1ebee81ad0de4
Gatsjy <Gatsjy@DESKTOP-4TT9522>
2020-09-21 22:10:48 +0900

    2020-09-21, 한주안, 등록 관련 api 만들기

---
+++ src/main/java/com/juan/chatting/springboot/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.juan.chatting.springboot.service.posts;
+
+import com.juan.chatting.springboot.domain.posts.PostsRepository;
+import com.juan.chatting.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
---
+++ src/main/java/com/juan/chatting/springboot/web/PostsApiController.java
@@ -0,0 +1,20 @@
+package com.juan.chatting.springboot.web;
+
+import com.juan.chatting.springboot.service.posts.PostsService;
+import com.juan.chatting.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+}
