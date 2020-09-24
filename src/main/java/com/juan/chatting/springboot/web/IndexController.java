4943f0c51c5bb533352bd6f05cc79368805e6037
Gatsjy <gkswndks123@naver.com>
2020-09-24 21:52:24 +0900

    2020-09-22, 한주안, 수정,저장 로직 구현

--- src/main/java/com/juan/chatting/springboot/web/IndexController.java
+++ src/main/java/com/juan/chatting/springboot/web/IndexController.java
@@ -1,10 +1,13 @@
 package com.juan.chatting.springboot.web;
 
 import com.juan.chatting.springboot.service.posts.PostsService;
+import com.juan.chatting.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RequiredArgsConstructor
 @Controller
@@ -21,5 +24,13 @@ public class IndexController {
     public String index(Model model){
         model.addAttribute("posts", postsService.findAllDesc());
         return "index";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
